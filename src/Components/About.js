ba94a3b765bbbafae1e365cd62cd6346edfc7ae5
pulkitpn <vlpulkit.773@gmail.com>
2022-01-12 03:40:33 +0530

    about page added

--- src/Components/About.js
+++ src/Components/About.js
@@ -9,13 +9,14 @@ const About = () => {
         <React.Fragment>
             <Navi />
             <div className="container-fluid">
-                <center><img src={anim} style={{ height: 300 }} className="img-fluid" /><br/><br/>
+                <center><img src={anim} style={{ height: 300 }} className="img-fluid" /><br /><br />
                     <span>Feel free to send us your feedback at:</span>
                     <p><span className="fa fa-envelope"></span>  <a href="mailto:pulkitn.mca20@rvce.edu.in">pulkitn.mca20@rvce.edu.in</a></p>
                     <p>or</p>
                     <p><span className="fa fa-envelope"></span>  <a href="mailto:anshulg.mca20@rvce.edu.in">anshulg.mca20@rvce.edu.in</a></p>
-                    <br/><br/><br/><br/>
-                    <span className="developed">DEVELOPED by PULKIT NAGAR and ANSHUL GOYAL</span>
+                    <footer>
+                        <span className="developed">DEVELOPED by PULKIT NAGAR and ANSHUL GOYAL</span>
+                    </footer>
 
                 </center>
             </div>
